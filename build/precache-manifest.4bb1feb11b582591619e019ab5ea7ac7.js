72a7d2b0ab397a600ec0a114ffca26c5ee1668cd
Kass Adams <kassadams@u.boisestate.edu>
2020-10-29 17:40:25 -0600

    deleted covidetracker19/, did 'npm run build'

---
+++ build/precache-manifest.4bb1feb11b582591619e019ab5ea7ac7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed734923488206596bf87c69cf110a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "792ae1a771c055057cc9",
+    "url": "/static/css/2.5b6e4d1f.chunk.css"
+  },
+  {
+    "revision": "85e57173b29e40168d46",
+    "url": "/static/css/main.a05f5b7a.chunk.css"
+  },
+  {
+    "revision": "792ae1a771c055057cc9",
+    "url": "/static/js/2.913ce51a.chunk.js"
+  },
+  {
+    "revision": "fd70b042948505ff0eb22221f60b4cb3",
+    "url": "/static/js/2.913ce51a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85e57173b29e40168d46",
+    "url": "/static/js/main.ed3c0feb.chunk.js"
+  },
+  {
+    "revision": "298caa4107a9afe4c377",
+    "url": "/static/js/runtime-main.b3f9e7cc.js"
+  },
+  {
+    "revision": "36146583ac0cc583670e48468a409fa5",
+    "url": "/static/media/covid1.36146583.jpg"
+  },
+  {
+    "revision": "3a0e9f0ea05117b5167027ec85ceaf2b",
+    "url": "/static/media/covid2.3a0e9f0e.jpg"
+  },
+  {
+    "revision": "73a4d84936391f023e26480d311c2628",
+    "url": "/static/media/covid3.73a4d849.mp4"
+  },
+  {
+    "revision": "5d770ac9b04e53ca9259cae524f68893",
+    "url": "/static/media/covid4.5d770ac9.jpg"
+  }
+]);
