043d265257f13f5211a285f8480a536077b1daa1
Alex Acevedo <alex.acevedo88@gmail.com>
2020-11-04 23:05:56 -0700

    Update NavbarElements.js

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -2,8 +2,6 @@ import styled from 'styled-components'
 import {Link as LinkR} from 'react-router-dom'
 import {Link as LinkS} from 'react-scroll'
 
-
-
 export const Nav = styled.nav`
     background: ${({scrollNav}) => (scrollNav ? '#000' : 'transparent')};
     height: 80px;
