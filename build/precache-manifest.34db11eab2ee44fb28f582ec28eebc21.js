a07389ca85802c977c4136b25a338425f10e2819
vannguyen <vannguyen599@u.boisestate.edu>
2020-11-12 21:10:19 -0700

    Added carousel

---
+++ build/precache-manifest.34db11eab2ee44fb28f582ec28eebc21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf4bdf3bd2ec7f58fec304471ab676b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75463ac9ceaf158355c2",
+    "url": "/static/css/2.5b6e4d1f.chunk.css"
+  },
+  {
+    "revision": "2cfda142103a9c8dce0e",
+    "url": "/static/css/main.b641c7d5.chunk.css"
+  },
+  {
+    "revision": "75463ac9ceaf158355c2",
+    "url": "/static/js/2.db30dac7.chunk.js"
+  },
+  {
+    "revision": "01f37e4e074a19a02968c19a498f5460",
+    "url": "/static/js/2.db30dac7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cfda142103a9c8dce0e",
+    "url": "/static/js/main.2d14ad3c.chunk.js"
+  },
+  {
+    "revision": "298caa4107a9afe4c377",
+    "url": "/static/js/runtime-main.b3f9e7cc.js"
+  },
+  {
+    "revision": "36146583ac0cc583670e48468a409fa5",
+    "url": "/static/media/covid1.36146583.jpg"
+  },
+  {
+    "revision": "3a0e9f0ea05117b5167027ec85ceaf2b",
+    "url": "/static/media/covid2.3a0e9f0e.jpg"
+  },
+  {
+    "revision": "73a4d84936391f023e26480d311c2628",
+    "url": "/static/media/covid3.73a4d849.mp4"
+  },
+  {
+    "revision": "5d770ac9b04e53ca9259cae524f68893",
+    "url": "/static/media/covid4.5d770ac9.jpg"
+  }
+]);
